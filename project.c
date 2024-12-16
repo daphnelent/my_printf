9aae3d89fa6f8279566aa69eeb1fd6e11067248e
daphnelent <daphnalent@gmail.com>
2024-12-16 00:02:29 -0500

    notes

---
+++ project.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// putc() or putchar()
+
+// format specifier syntax %[parameter][flags][width][.precision][length]type
+// parameter = n$ (specifies index, because parameters not sequential)
+// flags: - (left align), + (print plus for pos nums), space (print space for pos nums), 0 (when width specified prepend 0s instead of spaces), ' (apply thousands grouping to decimals), # (this one idg at all)
