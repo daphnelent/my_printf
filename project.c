3a58208762dbebec7fb15db5fcd05f4d5873799f
daphnelent <daphnalent@gmail.com>
2024-12-17 21:53:53 -0500

    printing types

--- project.c
+++ project.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// will use the following function to print integers for %d as putchar expects char values
+void print_int(int num);
+
+// will use the following function to convert integers to hex for %x
+void print_hex(int num);
+
+// will use the following function to print strings for %s
+void print_str(const char* str);
 
 // use putchar()
 
@@ -32,6 +42,60 @@
 // should return number of chars printed excluding null "\0" to end output
 
 
+void print_int(int num){
+    // for negatives
+    if (num < 0) {
+        // prepend negative sign
+        putchar('-');
+        // convert to positive to get numbers
+        num = -num;
+    }
 
+    // if the number is more than 1 digit, print the largest place digits first
+    if (num / 10) {
+        print_int(num / 10);
+    }
 
-// write test functions
+    // print the last digit (due to recursion this will print each digit)
+    putchar((num % 10 + '0'));
+}
+
+void print_hex(int num) {
+    // Fixed size for maximum of 8 hex digits (bc an int is 32 bits) + null terminator
+    char hexString[9];  
+    // create a string/array of all possible hexidecimal digits
+    const char hexChars[] = "0123456789abcdef";
+    int tempCounter = num;
+    int numDigits = 0;
+
+    // count number of digits
+    do {
+        numDigits++;
+        tempCounter /= 16;
+    } while (tempCounter > 0);
+
+    // insert null terminator at end of hexidecimal number
+    hexString[numDigits] = '\0';
+
+    // get the digits from rightmost to leftmost digit
+    for (int digit = numDigits - 1; digit >= 0; digit--) {
+        hexString[digit] = hexChars[num % 16];
+        num /= 16;
+    }
+
+    // print the chars
+    for (int hexChar = 0; hexString[hexChar] != '\0'; hexChar++){
+        putchar(hexString[hexChar]);
+
+    }
+    
+}
+
+void print_str(const char* str) {
+    // for each char in the str, output it
+    for (int i = 0; str[i] != '\0'; i++) {
+        putchar(str[i]);
+    }
+}
+
+// write test functions and put in ada
