c17b1953b5709c81eccc970ecf123503fb9d09f2
daphnelent <daphnalent@gmail.com>
2024-12-20 01:53:02 -0500

    printing int, char and hex

--- project.c
+++ project.c
@@ -2,24 +2,44 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-// will use the following function to print integers for %d as putchar expects char values
+
+// will use the following function to print integers for %d accounting for all the modifiers
+void print_formatted_int(int num, int width, int zero_padding, int precision, int left_align, int show_sign, int space_sign);
+// prints the actual number
 void print_int(int num);
 
-// will use the following function to convert integers to hex for %x
-void print_hex(int num);
+// get the width of the number so know how much padding needed 
+int width_int(int num);
+
+// will use the following function to print hex numbers for %x accounting for all the modifiers
+void print_formatted_hex(num, width, zero_padding, precision, left_align, hashtag);
+
+// get the width of the hex so know how much padding needed 
+int width_hex(unsigned int num);
 
 // will use the following function to print strings for %s
-void print_str(const char* str);
+void print_str(const char* str, int precision);
+
+// get the width of the str so know how much padding needed 
+int width_str(const char* str);
+
+// pad the formatted string to required width
+void pad_output(int totalWidth, int width, int zero_padding);
 
 // will use the following function to help parse the string
 const char* parsed_string(const char* format, va_list args);
 
 
 void my_printf(const char* format, ...) {
-    // va_list will have the variable arguments
-    va_list args;
+    // va_list will have the variable arguments, and copy will as well so we can count and store
+    va_list args, args_copy;
     // va_start initializes the va_list of argumets for use, format is last param before the elipses
     va_start(args, format);
+
+    // using a copy to count the number of args so can make a list of args
+    va_copy(args_copy, args); 
+  
+
 
     // loop through the format string
     while (*format != '\0') {
@@ -42,9 +62,13 @@ void my_printf(const char* format, ...) {
         else {
             putchar(*format);
         }
+        format++;
     }
     // end the variable arguments because done using them
     va_end(args);
+
+    // should return number of chars printed excluding null "\0" to end output
+
 }
 
 
@@ -52,12 +76,71 @@ void my_printf(const char* format, ...) {
 
 // implement 3 new things printf should do (1. switch to lowercase, 2. switch to uppercase, 3. switch to binary)
 
+// gets how many chars the number will take up
+int width_int(int num){
+    if (num == 0) return 1;
+    else {
+        // take off lowest digit each recursive call to count digits
+        if (num / 10 == 0)  { 
+            return 1;
+            }
+        return 1 + width_int(num / 10);
+    }
+}
 
+// gets how many chars the number will be in hex
+int width_hex(unsigned int num){
+    if (num == 0) return 1;
+    else {
+        // take off lowest digit each recursive call to count digits
+        if (num / 16 == 0)  { 
+            return 1;
+            }
+        return 1 + width_hex(num / 16);
+    }
+}
 
-// should return number of chars printed excluding null "\0" to end output
+// pad the output to get to the desired width
+void pad_output(int totalWidth, int width, int zero_padding){
+    char pad = ' ';
+    // if we need more charecters to meet the minimum width
+    if (totalWidth < width){
+        // check to see if zero padding flag is set
+        if (zero_padding == 1){
+            pad = '0';
+        }
+        // print the padding
+        for (int i = width; i > totalWidth; i--) {
+            putchar(pad);
+        }
+    
+    }
+}
 
+void print_formatted_int(int num, int width, int zero_padding, int precision, int left_align, int show_sign, int space_sign){
+    // get the number of digit charecters in out number
+    int digits = width_int(num);
+    int totalWidth;
+    // get the total width of the number itself
+    if (num < 0 || show_sign == 1 || space_sign == 1){
+        totalWidth = 1 + digits;
+    }
+    else {
+        totalWidth = digits;
+    }
 
-void print_int(int num){
+    // calculate how many extra digits may need to be added because of the precision modifier
+    precision -= digits;
+    // if extra digits are needed, add them to the total width of charecters 
+    if (precision > 0){
+        totalWidth += precision;
+    }
+
+    // if the left align flag is not set, insert the padding
+    if (left_align == 0){
+        pad_output(totalWidth, width, zero_padding);
+    }
+    
     // for negatives
     if (num < 0) {
         // prepend negative sign
@@ -65,6 +148,33 @@ void print_int(int num){
         // convert to positive to get numbers
         num = -num;
     }
+    // add necessary signs based on flags 
+    else {
+        if (show_sign == 1){
+            putchar('+');
+        }
+        else if (space_sign == 1) {
+            putchar(' ');
+        }
+    }
+
+    // add necessary digits to get to minimum as dictated by precision flag
+    while (precision > 0){
+        putchar('0');
+        precision--;
+    }
+
+    // print the actual number
+    print_int(num);
+
+    // if left align flag is set, insert padding here
+    if (left_align == 1){
+        pad_output(totalWidth, width, zero_padding);
+    }
+
+}
+
+void print_int(int num) {
 
     // if the number is more than 1 digit, print the largest place digits first
     if (num / 10) {
@@ -75,27 +185,63 @@ void print_int(int num){
     putchar((num % 10 + '0'));
 }
 
-void print_hex(int num) {
+void print_formatted_hex(num, width, zero_padding, precision, left_align, hashtag){
+    // these conditions undo zero padding in printf
+    if (precision >= 0) zero_padding = 0;
+    if (left_align == 1) zero_padding = 0;
+    // Treat the number as unsigned for correct hex conversion
+    unsigned int unsigned_num = (unsigned int)num;
     // Fixed size for maximum of 8 hex digits (bc an int is 32 bits) + null terminator
     char hexString[9];  
     // create a string/array of all possible hexidecimal digits
     const char hexChars[] = "0123456789abcdef";
-    int tempCounter = num;
-    int numDigits = 0;
+    int numDigits = width_hex(unsigned_num);
 
-    // count number of digits
-    do {
-        numDigits++;
-        tempCounter /= 16;
-    } while (tempCounter > 0);
-
-    // insert null terminator at end of hexidecimal number
+    // insert null terminator at end of hexidecimal number so we know when found the end
     hexString[numDigits] = '\0';
+
+    // update precision to be the number of zeros we will have to prepend to ensure enough digits
+    precision -= numDigits;
+
+    int totalWidth = numDigits;
+
+    // if we will be prepending zero digits, account for them in the width of chars
+    if (precision > 0){
+        totalWidth += precision;
+    }
+    
+    // if # flag is set, were going to add 0x which contributes to total width
+    if (hashtag == 1){
+        totalWidth += 2;
+    }
+
+    // insert spaces padding in output
+    if (left_align == 0 && zero_padding == 0){
+        pad_output(totalWidth, width, zero_padding);
+    }
+
+    // account for # flag
+    if (hashtag == 1){
+        putchar('0');
+        putchar('x');
+    }
+
+    // insert zeros padding in output
+    if (left_align == 0 && zero_padding == 1){
+        pad_output(totalWidth, width, zero_padding);
+    }
+
+    // account for precision digit requirements
+    while (precision > 0){
+        putchar('0');
+        precision--;
+    }
+    
 
     // get the digits from rightmost to leftmost digit
     for (int digit = numDigits - 1; digit >= 0; digit--) {
-        hexString[digit] = hexChars[num % 16];
-        num /= 16;
+        hexString[digit] = hexChars[unsigned_num % 16];
+        unsigned_num /= 16;
     }
 
     // print the chars
@@ -103,8 +249,13 @@ void print_hex(int num) {
         putchar(hexString[hexChar]);
 
     }
-    
+
+    if (left_align == 1){
+        pad_output(totalWidth, width, zero_padding);
+    }
 }
+
+
 
 const char* parsed_string(const char* format, va_list args) {
     int parameter = 0;
@@ -117,7 +268,7 @@ const char* parsed_string(const char* format, va_list args) {
     int space_sign = 0;
     char length = '\0';
     char type = '\0';
-    int precision = 0;
+    int precision = -1;
 
     // first check for parameter index
     // if we have a digit (might be parameter if followed by $)
@@ -132,7 +283,7 @@ const char* parsed_string(const char* format, va_list args) {
     // if there is no $ following the number, then it must've been the width or zero flag, there is no parameter
     else {
         if (parameter == 0) {
-            zero_padding = '0';
+            zero_padding = '1';
         }
         else {
             width = parameter;
@@ -150,6 +301,8 @@ const char* parsed_string(const char* format, va_list args) {
         }
         else if (*format == '-'){
             left_align = 1;
+            // left alignment undoes zero padding effects
+            zero_padding = 0;
         }
         else if (*format == ' '){
             if (!show_sign) {
@@ -183,23 +336,81 @@ const char* parsed_string(const char* format, va_list args) {
     // now handle precision
     if (*format == '.') {
         format++;
+        precision = 0;
         while (*format >= '0' && *format <= '9') {
                 precision = precision * 10 + (*format - '0');
                 format++;
         }
+    }
 
-    // now move to handling length
-    if (*format == 'h' || *format == 'l') {
-            length = *format;  
-            format++; 
+    // now move to handling length (POTENTIALLY ADD LL AND HH LATER)
+    if (*format == 'h'){
+        length = 'h';
+        format++;
+    }
+    
+    else if (*format == 'l'){
+        length = 'l';
+        format++;
         }
 
     // now move to handling type
     type = *format;
-    if (type == '\0') return format;  // Incomplete
 
-    format++;
 
+    if (type == 'd') {
+        if (length == 'h') {
+            short num = va_arg(args, int);
+            print_formatted_int(num, width, zero_padding, precision, left_align, show_sign, space_sign);
+        }
+        else if (length == 'l') {
+            long num = va_arg(args, long);
+            print_formatted_int(num, width, zero_padding, precision, left_align, show_sign, space_sign);
+        }
+        else {
+            int num = va_arg(args, int);
+            print_formatted_int(num, width, zero_padding, precision, left_align, show_sign, space_sign);
+        }
+    }
+    
+    else if (type == 'x'){
+        if (length == 'h') {
+            short num = va_arg(args, int);
+            print_formatted_hex(num, width, zero_padding, precision, left_align, hashtag);
+        }
+        else if (length == 'l') {
+            long num = va_arg(args, long);
+            print_formatted_hex(num, width, zero_padding, precision, left_align, hashtag);
+        }
+        else {
+            int num = va_arg(args, int);
+            print_formatted_hex(num, width, zero_padding, precision, left_align, hashtag);
+        }
+    }
+
+    else if (type == 'c'){
+        // ensure zero padding flag not set
+        zero_padding = 0;
+        if (left_align != 1) {
+            // width of 1 for 1 char
+            pad_output(1, width, zero_padding);
+            putchar(va_arg(args, char));
+        }
+        else {
+            putchar(va_arg(args, char));
+            pad_output(1, width, zero_padding);
+        }
+    }
+
+    else if (type == 's'){
+        // handle str
+    }
+    // if not a valid type, throw an error
+    else {
+        exit(EXIT_FAILURE);
+    }
+
+    return format;
 }
     // flags: (in order of can be overwritten to cant be)
         // # 0x added for hex
@@ -221,3 +432,8 @@ const char* parsed_string(const char* format, va_list args) {
 
 
 // write test functions and put in ada
+
+
+
+
+
